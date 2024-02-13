e23358645f8f594f2396d31f8ff63eef21031403
Alfonso Carrasco <ing.alfonso.carrasco@gmail.com>
2024-02-13 02:35:32 -0600

    feature(update, delete: Add UD, Update and Delete methods)

--- diaCuatro/reto.py
+++ diaCuatro/reto.py
@@ -30,7 +30,7 @@ while(flag):
     option = str(input(''))
 
     # Create User 🍤
-    if(option == 'A' or option == 'a'):
+    if(option.lower() == 'a'):
         name = str( input('Name: '))
         lastname = str( input('Lastname: ') )
         phone = int(input('Phone [10 dig]: '))
@@ -39,20 +39,81 @@ while(flag):
     
         new_flag = input('Return to the Menu? Y/N: ')
 
-        if(new_flag == 'N' or new_flag == 'n'):
+        if(new_flag.lower == 'n'):
             flag = False
     
     # Read Users 🍥
-    elif(option == 'B' or option == 'b'):
+    elif(option.lower() == 'b'):
+        print('            🔹🔹🔹🔹🔹🔹🔹🔹 LIST USERS 🔹🔹🔹🔹🔹🔹🔹🔹🔹 ')
         print('╔═══ ID ══╦════ NAME ════╦═══ LASTNAME ═══╦════ PHONE ═══╦═══ EMAIL ════╗')
         
+        if(len(users) == 0):
+            print('║                              Users not found                          ║')
         for user in users:
             print('║  ',user['id'], ' ║  ', user['name'], ' ║  ', user['lastname'], ' ║  ', user['phone'], ' ║ ', user['email'], ' ║')
         
         print('╚═══════════════════════════════════════════════════════════════════════╝')
         print('Total: ', len(users), ' Users registred 🥡.')
-    
-    # Update User 🥮
-    elif(option == 'C' or option == 'c'):
-        print('AQUI VA LA OPCION C')
+        new_flag_read = input('Return to the Menu? Y/N: ')
+        if(new_flag_read.lower() == 'n'):
+            flag = False
 
+    # Update User 🥮
+    elif(option.lower() == 'c'): 
+        
+        if(len(users) >= 1):
+            for user in users:
+                print('╔═══ ID ══╦════ NAME ════╦═══ LASTNAME ═══╦════ PHONE ═══╦═══ EMAIL ════╗')
+                print('║  ',user['id'], ' ║  ', user['name'], ' ║  ', user['lastname'], ' ║  ', user['phone'], ' ║ ', user['email'], ' ║')
+        
+                print('╚═══════════════════════════════════════════════════════════════════════╝')
+            id_user_update = int(input('Enter ID User to update: '))
+            flag_search = False
+            for user in users:
+                if(user['id'] == id_user_update):
+                    flag_search = True
+                    
+                    print('Actual name: 👉' + user['name'] + '👈 Enter new Name: ')
+                    new_name = input()
+                    print('Actual lastname: 👉' + user['lastname'] + '👈 Enter new Lastname:')
+                    new_lastname = input('')
+                    print('Actual phone: 👉', user['phone'], '👈 Enter new Phone: ')
+                    new_phone = int(input())
+                    print('Actual email: 👉' + user['email'] + '👈 Enter new Email: ')
+                    new_email = input()
+                    user['name'] = new_name
+                    user['lastname'] = new_lastname
+                    user['phone'] = new_phone
+                    user['email'] = new_email
+                    
+                    print('🍾 Update success 🍾')
+                    print('╔═══ ID ══╦════ NAME ════╦═══ LASTNAME ═══╦════ PHONE ═══╦═══ EMAIL ════╗')
+                    print('║  ',user['id'], ' ║  ', user['name'], ' ║  ', user['lastname'], ' ║  ', user['phone'], ' ║ ', user['email'], ' ║')
+        
+                    print('╚═══════════════════════════════════════════════════════════════════════╝')
+            if(flag_search == False):
+                print("Sorry, don't found user id: ", id_user_update)
+            
+        else:
+            print('🔴 🔴 Not found Users to update 🔴 🔴')
+        
+        new_flag_update  = input('Return to the Menu? Y/N: ')
+        if(new_flag_update.lower() == 'n'):
+            flag = False
+    # Delete User
+    elif(option.lower() == 'd'):
+        print('List Users')
+        for user in users:
+            print('╔═══ ID ══╦════ NAME ════╦═══ LASTNAME ═══╦════ PHONE ═══╦═══ EMAIL ════╗')
+            print('║  ',user['id'], ' ║  ', user['name'], ' ║  ', user['lastname'], ' ║  ', user['phone'], ' ║ ', user['email'], ' ║')
+            print('╚═══════════════════════════════════════════════════════════════════════╝')
+            id_user_delete = int(input('Enter ID User to delete: '))
+            for user in users:
+                if(user['id'] == id_user_delete):
+                    users.remove(user)
+
+            print('Congratulations, Remove user 🍨')
+
+            new_flag_delete = input('Return to the Menu? Y/N: ')
+            if(new_flag_delete.lower() == 'n'):
+                flag = false
